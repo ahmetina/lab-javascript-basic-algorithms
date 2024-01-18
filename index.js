1c84c32ce0f8ee147bab3731b8dbe74aa3cade24
Ahmet_İnan <ahmet92@gmail.com>
2024-01-18 23:29:04 +0300

    lab begining

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // Iteration 1: Names and Input
+let hacker1="driversName"
+console.log("Drivers name is"+hacker1)
+let hacker2="navigatorsName"
+console.log("Navigators name is" + hacker2)
+
 
 
 // Iteration 2: Conditionals
-
-aasdas
+if(hacker1.length>){
+console.log("The driver has  the longest name, it has"+hacker1.length+"characters")
+}
+else if(hacker2.length>hacker1.length){
+    console.log("The driver has  the longest name, it has"+hacker2.length+"characters")
+}
+else{
+    console.log("Both has equally long names,"+hacker1.length+"characters")
+}
 // Iteration 3: Loops
